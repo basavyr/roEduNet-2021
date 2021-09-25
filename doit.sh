a1b92cdfb446804999ce5747cd3257a0b6c69510
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-09-25 07:49:05 +0300

    improves script

--- doit.sh
+++ doit.sh
@@ -1,9 +1,25 @@
+echo ''
+echo '*********************'
+echo 'Starting compilation of the latex file...'
+echo '*********************'
 pdflatex main
 bibtex main
 pdflatex main
+echo ''
+echo '*********************'
+echo 'Generating final .pdf file...'
+echo '*********************'
 pdflatex main
+echo ''
+echo '*********************'
+echo 'Removing extra files...'
+echo '*********************'
 rm main.log
-rm main.pdf
 rm main.aux
 rm main.blg
 rm main.out
+echo ''
+echo '*********************'
+echo 'Script finished executing...'
+echo '*********************'
+echo '😊'
